52425a684107e9d5c0a0f18b7950cb77f23bda08
ZYGAL0 <efimov.se@phystech.edu>
2019-10-20 14:47:54 +0300

    Count perimeter and area of figure

---
+++ set_of_figures/cmake-build-debug/CMakeFiles/set_of_figures.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/set_of_figures.dir/set_of_figures.cpp.o"
+  "set_of_figures"
+  "set_of_figures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/set_of_figures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
